091244ff420e9667a728a4540dda66abc376fb5a
Zafer Balkan <zafer@zaferbalkan.com>
2026-06-15 09:17:35 +0300

    Extracted ADREPLDC class to its own file

--- ADReplStatusForm.cs
+++ ADReplStatusForm.cs
@@ -931,21 +931,4 @@ namespace ADReplStatus
         }
     }
 
-    public class ADREPLDC
-    {
-        public string Name;
-
-        public string DomainName;
-
-        public bool DiscoveryIssues = false;
-
-        public string Site;
-
-        public string IsGC;
-
-        public string IsRODC;
-
-        public List<ReplicationNeighbor> ReplicationPartners = new List<ReplicationNeighbor>();
-    }
-
 }
